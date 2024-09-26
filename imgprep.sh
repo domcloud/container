4d7aa3a51aedd5388b30f90c51e87d0ade386c4c
Wildan M <willnode@wellosoft.net>
2024-09-26 08:00:59 +0700

    KVM notes

--- imgprep.sh
+++ imgprep.sh
@@ -1,7 +1,7 @@
 export PACKER_CACHE_DIR=$PWD/output/cache
 export PACKER_LOG_PATH=$PWD/output/packer.log
 export PACKER_LOG=1
-ARCH=$(uname -m)
+ARCH=$(uname -m) # due to KVM
 rm -rf $PWD/output/image-$ARCH
 packer init vm.$ARCH.pkr.hcl
 packer build vm.$ARCH.pkr.hcl
