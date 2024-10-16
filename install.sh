8a8430fffed792acdeb29d4f5274dc41b6b72658
Wildan M <willnode@wellosoft.net>
2024-10-16 23:42:34 +0700

    Fix php

--- install.sh
+++ install.sh
@@ -73,7 +73,7 @@ cd .. ; rm -rf rdfind-1.6.0 rdfind-1.6.0.tar.gz
 # Misc
 pip3 install pipenv awscli
 dnf -y remove nodejs-docs clang flatpak open-sans-fonts rubygem-rdoc gl-manpages
-systemctl enable webmin mariadb nginx postgresql-$PG {ip,ip6}tables fail2ban named php{74,81,82,83}-php-fpm earlyoom
+systemctl enable webmin mariadb nginx postgresql-$PG {ip,ip6}tables fail2ban named php{74,83}-php-fpm earlyoom
 
 # Cleanup
 # nobest due https://cloudlinux.zendesk.com/hc/en-us/articles/15731606500124
