fa46c86a5586da41ab9cd0ab9dd319bd99c765a0
Wildan M <willnode@wellosoft.net>
2024-11-26 18:15:43 +0700

    Avoid copr and tidyup

--- install.sh
+++ install.sh
@@ -7,13 +7,11 @@ export TERM=xterm-256color
 dnf -y install epel-release http://rpms.remirepo.net/enterprise/remi-release-9.rpm && dnf config-manager --enable crb
 dnf -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-$(uname -m)/pgdg-redhat-repo-latest.noarch.rpm
 curl -fsSL https://software.virtualmin.com/gpl/scripts/virtualmin-install.sh | sh -s -- --setup --verbose
-curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo > /etc/yum.repos.d/yarn.repo
+curl -sSL https://dl.yarnpkg.com/rpm/yarn.repo > /etc/yum.repos.d/yarn.repo
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 dnf config-manager --disable virtualmin pgdg{16,15,14,13,12}
 
 # Modules
-dnf -y module reset nginx
-dnf -y module enable nginx:1.24
 dnf -y module reset nodejs
 dnf -y module enable nodejs:22
 dnf -y module reset mariadb
@@ -22,7 +20,7 @@ dnf -y module enable mariadb
 PG=17
 
 # Tools
-dnf -y install awscli btop bzip2 certbot clang cmake gcc-c++ git ncdu htop iftop ipset jq lsof make nano ninja-build ncurses npm nodejs ruby rsync screen socat strace tar time tmux vim wget whois xz yarn zstd \
+dnf -y install awscli btop bzip2 certbot clang cmake gcc-c++ git ncdu htop iftop ipset jq lsof make nano ninja-build ncurses npm nodejs ripgrep ruby rsync screen socat strace tar time tmux vim wget whois xz yarn zstd \
   lib{curl,ffi,sqlite3x,tool-ltdl,md,yaml}-devel {brotli,bzip2,mesa-libGL,nettle,openldap,pcre2,perl,python,readline,ruby,xmlsec1,xmlsec1-openssl}-devel python3-pip rubygem-{json,rack,rake} \
   libreport-filesystem {langpacks,glibc-langpack}-en perl-{DBD-Pg,DBD-mysql,LWP-Protocol-https,macros,DateTime,Crypt-SSLeay,Text-ASCIITable,IO-Tty,XML-Simple} \
   earlyoom fail2ban-server iptables-services postfix mariadb-server wbm-virtual-server wbm-virtualmin-{nginx,nginx-ssl} virtualmin-config bind sudo \
@@ -31,11 +29,8 @@ ln -s /usr/bin/gcc /usr/bin/$(uname -m)-linux-gnu-gcc # fix pip install with nat
 
 # NGINX
 git clone https://github.com/domcloud/nginx-builder/ /usr/local/lib/nginx-builder
-cd /usr/local/lib/nginx-builder/
-make install
-make clean
+cd /usr/local/lib/nginx-builder/ && make install && make clean && cd /root
 ln -s /usr/local/sbin/nginx /usr/sbin/nginx
-cd /root
 
 # PHP
 dnf -y install php{74,83}-php-{bcmath,cli,common,devel,fpm,gd,imap,intl,mbstring,mysqlnd,opcache,pdo,pecl-mongodb,pecl-redis,pecl-zip,pgsql,process,sodium,soap,xml}
@@ -69,31 +64,36 @@ modprobe ip_tables && echo "ip_tables" >> /etc/modules
 PATHMAN=pathman-v0.6.0-linux-$( [ "$(uname -m)" = "aarch64" ] && echo "arm64" || echo "amd64_v1" )
 wget -O pathman.tar.gz https://github.com/therootcompany/pathman/releases/download/v0.6.0/$PATHMAN.tar.gz
 tar -xf pathman.tar.gz && mv $PATHMAN /usr/local/bin/pathman && rm -f pathman.tar.gz
-# NVIM + NvChad Support
+# NVIM for NvChad
 NVIM_V=0.10.2
 if [ "$(uname -m)" = "x86_64" ]; then
-  curl -LO https://github.com/neovim/neovim/releases/download/v$NVIM_V/nvim-linux64.tar.gz
-  tar -xzf nvim-linux64.tar.gz && chown -R root:root nvim-linux64 && rsync -a nvim-linux64/ /usr/local/ && rm -rf nvim-linux64*
+  curl -sSLO https://github.com/neovim/neovim/releases/download/v$NVIM_V/nvim-linux64.tar.gz
+  tar -xf nvim-linux64.tar.gz && chown -R root:root nvim-linux64 && rsync -a nvim-linux64/ /usr/local/ && rm -rf nvim-linux64*
 else
-  curl -LO https://github.com/neovim/neovim/archive/refs/tags/v$NVIM_V.tar.gz
-  tar -xzf v$NVIM_V.tar.gz && mv neovim-$NVIM_V neovim && rm -f v$NVIM_V.tar.gz
+  curl -sSLO https://github.com/neovim/neovim/archive/refs/tags/v$NVIM_V.tar.gz
+  tar -xf v$NVIM_V.tar.gz && mv neovim-$NVIM_V neovim && rm -f v$NVIM_V.tar.gz
   cd neovim && make CMAKE_BUILD_TYPE=Release && make install && cd .. && rm -rf neovim
 fi
-dnf -y copr enable atim/lazygit && dnf -y install ripgrep lazygit
+# Lazygit for NVIM
+LAZYGIT=lazygit_0.44.1_Linux_$( [ "$(uname -m)" = "aarch64" ] && echo "arm64" || echo "x86_64" )
+curl -sSLO https://github.com/jesseduffield/lazygit/releases/download/v0.44.1/$LAZYGIT.tar.gz
+tar -xf $LAZYGIT.tar.gz && mv lazygit /usr/local/bin/ && rm -f $LAZYGIT.tar.gz
+
 # Neofetch (Forked)
 wget -O /usr/local/bin/neofetch https://github.com/hykilpikonna/hyfetch/raw/1.4.11/neofetch
-chmod +x /usr/local/bin/neofetch
 # Rdfind
-wget https://rdfind.pauldreik.se/rdfind-1.6.0.tar.gz
-tar -xf rdfind-1.6.0.tar.gz ; cd rdfind-1.6.0
+RDFIND=rdfind-1.6.0
+wget https://rdfind.pauldreik.se/$RDFIND.tar.gz
+tar -xf $RDFIND.tar.gz ; cd $RDFIND
 ./configure --disable-debug ; make install
-cd .. ; rm -rf rdfind-1.6.0 rdfind-1.6.0.tar.gz
+cd .. ; rm -f $RDFIND*
 # Misc
 pip3 install pipenv
 dnf -y mark install ipset
 dnf -y remove lynx gcc-toolset-13-* nodejs-docs clang flatpak open-sans-fonts rubygem-rdoc gl-manpages
 ln -s /usr/lib/systemd/system/postgresql-$PG.service /usr/lib/systemd/system/postgresql.service
 systemctl enable webmin mariadb postgresql-$PG {ip,ip6}tables fail2ban named php{74,83}-php-fpm earlyoom
+chmod +x /usr/local/bin/* && chown root:root /usr/local/bin/*
 
 # Cleanup
 # nobest since sometimes broken like https://cloudlinux.zendesk.com/hc/en-us/articles/15731606500124
