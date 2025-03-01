c732e70fafd9394aa2bebdeb4e0fefdbb47cff92
Wildan M <willnode@wellosoft.net>
2025-03-01 09:24:11 +0700

    Fix install

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -6,11 +6,11 @@ export CODENAME=$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAM
 
 # Repository
 
+apt-get update; apt-get -y install ca-certificates curl
 curl -fsSL https://deb.nodesource.com/setup_22.x | bash - 
 curl -fsSL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-wget -fsSL http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
-curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+curl -fsSL http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
 
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 echo "deb https://download.docker.com/linux/ubuntu $CODENAME stable" | tee /etc/apt/sources.list.d/docker.list
@@ -20,10 +20,9 @@ add-apt-repository ppa:ondrej/php
 
 # Installations
 apt-get update
-apt-get -y install ca-certificates pgdg-keyring
 apt-get -y install bzip2 bison btop clang certbot cmake git ncdu htop iftop ipset jq lsof make nano ninja-build ncurses-bin nodejs patch ripgrep ruby rsync screen socat strace tar time tmux vim wget whois xz-utils zstd \
         libcurl4-openssl-dev libffi-dev libsqlite3-dev libtool libssl-dev libyaml-dev brotli libbz2-dev libgl1-mesa-dev libldap2-dev libpcre2-dev python3-dev libreadline-dev redis-server libxmlsec1-dev python3-pip ruby-json ruby-rack \
-        language-pack-en libc-bin libdbd-pg-perl libdbd-mysql-perl liblwp-protocol-https-perl libdatetime-perl libcrypt-ssleay-perl libtext-asciitable-perl libio-tty-perl libxml-simple-perl libpq-dev
+        language-pack-en libc-bin libdbd-pg-perl libdbd-mysql-perl liblwp-protocol-https-perl libdatetime-perl libcrypt-ssleay-perl libtext-asciitable-perl libio-tty-perl libxml-simple-perl libpq-dev pgdg-keyring
 apt-get -y install webmin valkey-server earlyoom fail2ban nftables postfix bind9 sudo openssh-server systemd-container
 
 # Docker
