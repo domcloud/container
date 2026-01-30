724876b9f13d0fc13343342a5b1b5f536be591a8
Wildan M <willnode@wellosoft.net>
2026-01-30 17:19:19 +0700

    Bump nginx

--- install-extra.sh
+++ install-extra.sh
@@ -5,7 +5,7 @@ cd /root
 # NGINX
 BUILDER_DIR=/usr/local/lib/nginx-builder; [ ! -d "$BUILDER_DIR" ] && \
 git clone https://github.com/domcloud/nginx-builder/ $BUILDER_DIR || git -C $BUILDER_DIR pull
-cd $BUILDER_DIR/ && make install DOWNLOAD_V=1.2.0 && make clean && cd /root
+cd $BUILDER_DIR/ && make install DOWNLOAD_V=1.3.0 && make clean && cd /root
 ln -fs /usr/local/sbin/nginx /usr/sbin/nginx # nginx compatibility
 
 # Composer
